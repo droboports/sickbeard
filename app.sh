97d429025053a636d716543d31694d02ccb01145
Ricardo Padilha <ricardospadilha@gmail.com>
2015-07-09 13:59:00 -0700

    Updated to sickbeard 0.507.63 dev branch, build and app scripts.

--- app.sh
+++ app.sh
@@ -3,48 +3,82 @@ _build_cheetah() {
 local VERSION="2.4.4"
 local FILE="Cheetah-${VERSION}-py2.7-linux-armv7l.egg"
 local URL="https://github.com/droboports/python-cheetah/releases/download/v${VERSION}/${FILE}"
-local XPYTHON=~/xtools/python2/${DROBO}
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
 
 _download_file "${FILE}" "${URL}"
 mkdir -p "${DEST}/lib/python2.7/site-packages"
-_PYTHON_HOST_PLATFORM="linux-armv7l" PYTHONPATH="${DEST}/lib/python2.7/site-packages" ${XPYTHON}/bin/easy_install --prefix="${DEST}" --always-copy "download/${FILE}"
+PYTHONPATH="${DEST}/lib/python2.7/site-packages" \
+  "${XPYTHON}/bin/easy_install" --prefix="${DEST}" --always-copy "download/${FILE}"
+}
+
+### CFFI ###
+_build_cffi() {
+# also installs pycparser
+local VERSION="1.1.2"
+local FILE="cffi-${VERSION}-py2.7-linux-armv7l.egg"
+local URL="https://github.com/droboports/python-cffi/releases/download/v${VERSION}/${FILE}"
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
+
+_download_file "${FILE}" "${URL}"
+mkdir -p "${DEST}/lib/python2.7/site-packages"
+PYTHONPATH="${DEST}/lib/python2.7/site-packages" \
+  "${XPYTHON}/bin/easy_install" --prefix="${DEST}" --always-copy "download/${FILE}"
+}
+
+### CRYPTOGRAPHY ###
+_build_cryptography() {
+# also installs ipaddress enum34 six pyasn1 idna
+# depends on cffi
+local VERSION="0.9.1"
+local FILE="cryptography-${VERSION}-py2.7-linux-armv7l.egg"
+local URL="https://github.com/droboports/python-cryptography/releases/download/v${VERSION}/${FILE}"
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
+
+_download_file "${FILE}" "${URL}"
+mkdir -p "${DEST}/lib/python2.7/site-packages"
+PYTHONPATH="${DEST}/lib/python2.7/site-packages" \
+  "${XPYTHON}/bin/easy_install" --prefix="${DEST}" --always-copy "download/${FILE}"
 }
 
 ### PYOPENSSL ###
 _build_pyopenssl() {
-local VERSION="0.13"
-local FILE="pyOpenSSL-${VERSION}-py2.7-linux-armv7l.egg"
+# depends on cryptography
+local VERSION="0.15.1"
+local FILE="pyOpenSSL-${VERSION}-py2.7.egg"
 local URL="https://github.com/droboports/python-pyopenssl/releases/download/v${VERSION}/${FILE}"
-local XPYTHON=~/xtools/python2/${DROBO}
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
 
 _download_file "${FILE}" "${URL}"
 mkdir -p "${DEST}/lib/python2.7/site-packages"
-_PYTHON_HOST_PLATFORM="linux-armv7l" PYTHONPATH="${DEST}/lib/python2.7/site-packages" ${XPYTHON}/bin/easy_install --prefix="${DEST}" --always-copy "download/${FILE}"
+PYTHONPATH="${DEST}/lib/python2.7/site-packages" \
+  "${XPYTHON}/bin/easy_install" --prefix="${DEST}" --always-copy "download/${FILE}"
 }
 
 ### SICKBEARD ###
 _build_sickbeard() {
-local BRANCH="master"
-local FOLDER="app"
-local URL="https://github.com/midgetspy/Sick-Beard.git"
+# 63rd commit after 0.507 in the development branch
+local VERSION="0.507.63"
+local COMMIT="7cfff7f0fa69946d37be8762b71b88ac60f10505"
+local FOLDER="Sick-Beard-${COMMIT}"
+local FILE="${FOLDER}.zip"
+local URL="https://github.com/midgetspy/Sick-Beard/archive/${COMMIT}.zip"
 
-_download_git "${BRANCH}" "${FOLDER}" "${URL}"
+_download_zip "${FILE}" "${URL}" "${FOLDER}"
 mkdir -p "${DEST}/app"
-cp -avR "target/${FOLDER}"/* "${DEST}/app/"
-
-#local VERSION="506"
-#local FOLDER="Sick-Beard-build-${VERSION}"
-#local FILE="build-${VERSION}.tar.gz"
-#local URL="https://github.com/midgetspy/Sick-Beard/archive/${FILE}"
-
-#_download_tgz "${FILE}" "${URL}" "${FOLDER}"
-#mkdir -p "${DEST}/app"
-#cp -avR "target/${FOLDER}"/* "${DEST}/app/"
+sed -e "s|ssl_certificate_chain = None|ssl_certificate_chain = '/mnt/DroboFS/Shares/DroboApps/python2/etc/ssl/certs/ca-certificates.crt'|g" -i "target/${FOLDER}/cherrypy/_cpserver.py"
+cp -vfaR "target/${FOLDER}/"* "${DEST}/app/"
+echo "SICKBEARD_VERSION = \"${VERSION} ${COMMIT:0:7}\"" > "${DEST}/app/sickbeard/version.py"
 }
 
 ### BUILD ###
 _build() {
   _build_cheetah
+  _build_cffi
+  _build_cryptography
   _build_pyopenssl
   _build_sickbeard
   _package
--- src/dest/service.sh
+++ src/dest/service.sh
@@ -5,95 +5,43 @@
 # import DroboApps framework functions
 . /etc/service.subr
 
-# DroboApp framework version
-framework_version="2.0"
-
-# app description
+framework_version="2.1"
 name="sickbeard"
-version="0.506"
+version="0.507.63"
 description="Internet PVR for TV shows"
+depends="python2"
+webui=":8082/"
 
-# framework-mandated variables
-pidfile="/tmp/DroboApps/${name}/pid.txt"
-logfile="/tmp/DroboApps/${name}/log.txt"
-statusfile="/tmp/DroboApps/${name}/status.txt"
-errorfile="/tmp/DroboApps/${name}/error.txt"
-
-# app-specific variables
-prog_dir=$(dirname $(readlink -fn ${0}))
-python="${DROBOAPPS_DIR}/python2/bin/python"
+prog_dir="$(dirname "$(realpath "${0}")")"
+daemon="${DROBOAPPS_DIR}/python2/bin/python"
 conffile="${prog_dir}/data/config.ini"
+tmp_dir="/tmp/DroboApps/${name}"
+pidfile="${tmp_dir}/pid.txt"
+logfile="${tmp_dir}/log.txt"
+statusfile="${tmp_dir}/status.txt"
+errorfile="${tmp_dir}/error.txt"
 
-# script hardening
+# backwards compatibility
+if [ -z "${FRAMEWORK_VERSION:-}" ]; then
+  framework_version="2.0"
+  . "${prog_dir}/libexec/service.subr"
+fi
+
+start() {
+  PATH="${prog_dir}/libexec:${DROBOAPPS_DIR}/git/bin:${PATH}" \
+  PYTHONPATH="${prog_dir}/lib/python2.7/site-packages" \
+  "${daemon}" "${prog_dir}/app/SickBeard.py" --datadir="${prog_dir}/data" --pidfile="${pidfile}" --nolaunch --daemon
+}
+
+# boilerplate
+if [ ! -d "${tmp_dir}" ]; then mkdir -p "${tmp_dir}"; fi
+exec 3>&1 4>&2 1>> "${logfile}" 2>&1
+STDOUT=">&3"
+STDERR=">&4"
+echo "$(date +"%Y-%m-%d %H-%M-%S"):" "${0}" "${@}"
 set -o errexit  # exit on uncaught error code
 set -o nounset  # exit on unset variable
 set -o pipefail # propagate last error code on pipe
+set -o xtrace   # enable script tracing
 
-# ensure log folder exists
-logfolder="$(dirname ${logfile})"
-if [[ ! -d "${logfolder}" ]]; then mkdir -p "${logfolder}"; fi
-
-# redirect all output to logfile
-exec 3>&1 1>> "${logfile}" 2>&1
-
-# log current date, time, and invocation parameters
-echo $(date +"%Y-%m-%d %H-%M-%S"): ${0} ${@}
-
-# enable script tracing
-set -o xtrace
-
-# _is_running
-# args: path to pid file
-# returns: 0 if pid is running, 1 if not running or if pidfile does not exist.
-_is_running() {
-  /sbin/start-stop-daemon -K -s 0 -x "${python}" -p "${pidfile}" -q
-}
-
-_create_config() {
-  if [[ ! -f "${conffile}" ]]; then
-    cp "${conffile}.default" "${conffile}"
-  fi
-}
-
-start() {
-  _create_config
-  rm -f "${pidfile}"
-  PATH="${prog_dir}/libexec:${DROBOAPPS_DIR}/git/bin:${PATH}" PYTHONPATH="${prog_dir}/lib/python2.7/site-packages" "${python}" "${prog_dir}/app/SickBeard.py" --datadir="${prog_dir}/data" --pidfile="${pidfile}" --nolaunch --daemon
-}
-
-_service_start() {
-  set +e
-  set +u
-  if _is_running "${pidfile}"; then
-    echo ${name} is already running >&3
-    return 1
-  fi
-  start_service
-  set -u
-  set -e
-}
-
-_service_stop() {
-  /sbin/start-stop-daemon -K -x "${python}" -p "${pidfile}" -v || echo "${name} is not running" >&3
-}
-
-_service_restart() {
-  service_stop
-  sleep 3
-  service_start
-}
-
-_service_status() {
-  status >&3
-}
-
-_service_help() {
-  echo "Usage: $0 [start|stop|restart|status]" >&3
-  set +e
-  exit 1
-}
-
-case "${1:-}" in
-  start|stop|restart|status) _service_${1} ;;
-  *) _service_help ;;
-esac
+main "${@}"
